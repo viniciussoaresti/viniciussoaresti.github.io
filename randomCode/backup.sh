96c8238768ba10cb63e63557c3605a226195e4bf
Vinícius Soares <viniciussoaresti@gmail.com>
2025-11-03 14:50:59 -0300

    feat: reorder outputs and files

--- randomCode/backup.sh
+++ randomCode/backup.sh
@@ -1,17 +1,18 @@
 echo "Making sure destination directories exist."
-mkdir -p /g/cDisk/downloads/ &
-mkdir -p /g/cDisk/eclipse-workspace/ &
-mkdir -p /g/cDisk/pictures/ &
-mkdir -p /g/cDisk/videos/ &
-mkdir -p /g/eDisk/osuSongs/ &
-mkdir -p /g/fDisk/Downloads &
+touch backup.txt
+mkdir -p /g/cDisk/downloads/ >> backup.txt 2>&1 &
+mkdir -p /g/cDisk/eclipse-workspace/ >> backup.txt 2>&1 &
+mkdir -p /g/cDisk/pictures/ >> backup.txt 2>&1 &
+mkdir -p /g/cDisk/videos/ >> backup.txt 2>&1 &
+mkdir -p /g/eDisk/osuSongs/ >> backup.txt 2>&1 &
+mkdir -p /g/fDisk/Downloads >> backup.txt 2>&1 &
 wait
 echo "Check done, starting copies!"
-cp -r /c/Users/vinic/Downloads/* /g/cDisk/downloads/ &
-cp -r /c/Users/vinic/eclipse-workspace/* /g/cDisk/eclipse-workspace/ &
-cp -r /c/Users/vinic/Pictures/* /g/cDisk/pictures/ &
-cp -r /c/Users/vinic/Videos/* /g/cDisk/videos/ &
-cp -r /e/Jogos/osu!/Songs/* /e/eDisk/osuSongs/ &
-cp -r /f/hdFiles/* /g/fDisk/Downloads/ &
+cp -r /c/Users/vinic/Downloads/* /g/cDisk/downloads/ >> backup.txt 2>&1 &
+cp -r /c/Users/vinic/eclipse-workspace/* /g/cDisk/eclipse-workspace/ >> backup.txt 2>&1 &
+cp -r /c/Users/vinic/Pictures/* /g/cDisk/pictures/ >> backup.txt 2>&1 &
+cp -r /c/Users/vinic/Videos/* /g/cDisk/videos/ >> backup.txt 2>&1 &
+cp -r /e/Jogos/osu!/Songs/* /g/eDisk/osuSongs/ >> backup.txt 2>&1 &
+cp -r /f/hdFiles/* /g/fDisk/Downloads/ >> backup.txt 2>&1 &
 wait
 echo "All done!"
